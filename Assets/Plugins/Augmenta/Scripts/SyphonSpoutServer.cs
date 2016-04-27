207ed9a9b0c9f03049a72cf27ecb2467b148e71d
Baptiste Moizard <bmoizard@theoriz.com>
2016-04-27 12:10:56 +0200

    Refactored SyphonSpoutServer script and added funnel support

--- Assets/Plugins/Augmenta/Scripts/SyphonSpoutServer.cs
+++ Assets/Plugins/Augmenta/Scripts/SyphonSpoutServer.cs
@@ -21,51 +21,22 @@ public class SyphonSpoutServer : MonoBehaviour {
 	private int oldRenderWidth;
 	private int oldRenderHeight;
 
+	#if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+	private enum GraphicServer{
+		SYPHON,
+		FUNNEL
+	};
+
+	private GraphicServer graphicServer = GraphicServer.SYPHON;
+	#endif
+
 	void Start () {
 
 		LoadSettings ();
 		Debug.Log ("Render size after load settings : " + renderWidth + "x" + renderHeight);
 
 		// Init Syphon or Spout server
-		#if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
-		// Add syphon components
-		gameObject.AddComponent<Syphon>();
-		gameObject.AddComponent<SyphonServerTextureCustomResolution>();
-
-		#elif UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
-		// Create a new camera for Spout, based on this camera
-		// because Spout can't send a render texture and show the scene in app window with the same camera
-		// This instantiation is made in Start function, to prevent cloned camera 
-		// to replicate itself through this script before it is destroyed.
-		GameObject spoutCamera = Instantiate(gameObject);
-
-		// Remove useless cloned components
-		Destroy(spoutCamera.GetComponent<SyphonSpoutServer>());
-		Destroy(spoutCamera.GetComponent<AudioListener>());
-		Destroy(spoutCamera.GetComponent<GUILayer>());
-
-		// Set name and tag 
-		spoutCamera.name = "Camera Spout Sender";
-		spoutCamera.tag = "SpoutCamera";
-
-		// Make spout sender camera child of main camera
-		spoutCamera.transform.parent = this.transform;
-
-		// Add spout components 
-		gameObject.AddComponent<Spout.Spout>();	// Instantiate Spout script on main camera to prevent crash when exiting app
-		spoutCamera.AddComponent<Spout.SpoutSender>();
-		spoutCamera.AddComponent<Spout.InvertCamera>();
-
-		// Setup spout components
-		spoutCamera.GetComponent<Spout.SpoutSender>().sharingName = Application.productName;
-		// Create a new render texture
-		RenderTexture targetTexture = new RenderTexture(renderWidth, renderHeight, 24);
-		// Set it to the camera 
-		spoutCamera.GetComponent<Camera>().targetTexture = targetTexture;
-		// And set it to Spout
-		spoutCamera.GetComponent<Spout.SpoutSender>().texture = targetTexture;
-
-		#endif
+		SetupGraphicServer ();
 
 		// Init values
 		oldRenderWidth = renderWidth;
@@ -111,40 +82,7 @@ public class SyphonSpoutServer : MonoBehaviour {
 			}
 		}
 
-		#if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
-		// Apply changes to syphon server
-		ResizeSyphonRenderTexture ();
-
-		#elif UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
-		// Apply changes to syphon server
-		GameObject spoutChild = null;
-		foreach (Transform child in gameObject.transform) {
-			if(child.CompareTag("SpoutCamera")){
-				spoutChild = child.gameObject;
-				break;
-			}
-		}
-		Spout.SpoutSender spoutSender = spoutChild.GetComponent<Spout.SpoutSender>();
-		Camera camera = spoutChild.GetComponent<Camera>();
-
-		// Disable Spout while updating targettexture to prevent crash
-		spoutSender.enabled = false;
-
-		// Create a new render texture because we can't reallocate an already existing one
-		// Store camera target texture in temp variable to be able to release it while not in use by the camera
-		RenderTexture targetTexture = camera.targetTexture;
-		camera.targetTexture = null;
-		targetTexture.Release();
-		targetTexture = new RenderTexture(renderWidth, renderHeight, 24);
-		// Set it to the camera 
-		camera.targetTexture = targetTexture;
-		// Set it to spout sender
-		spoutSender.texture = targetTexture;
-
-		// Enable spout sender
-		spoutSender.enabled = true;
-
-		#endif
+		ResizeRenderTexture ();
 
 		// If aspect ratio not locked, compute it 
 		if (!lockAspectRatio) {
@@ -181,12 +119,150 @@ public class SyphonSpoutServer : MonoBehaviour {
 
 	}
 
+	void SetupGraphicServer(){
+		#if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+		
+		// Check current Unity version
+		// < 5.3
+		if(float.Parse( Application.version.Substring(0,3))<5.3){ //test the current Unity version. if it's oldest than 5.3 use syphon, else use funnel
+			// Use Syphon (because have some issues on version >5.2 when writing this code)
+			graphicServer = GraphicServer.SYPHON;
+		}
+		// >= 5.3
+		else{
+			// Use funnel, compatible with 5.3+ and latest OpenGL backend
+			graphicServer = GraphicServer.FUNNEL;
+		}
+		
+		switch(graphicServer){
+		case GraphicServer.SYPHON:
+			SetupSyphonServer();
+			break;
+			
+		case GraphicServer.FUNNEL:
+			SetupFunnelServer();
+			break;
+			
+		default:
+			break;
+		}
+		
+		#elif UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+
+		SetupSpoutServer();
+		#endif
+	}
+
+	void ResizeRenderTexture(){
+		#if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+		// Apply changes to syphon server
+		switch(graphicServer){
+		case GraphicServer.SYPHON:
+			ResizeSyphonRenderTexture();
+			break;
+			
+		case GraphicServer.FUNNEL:
+			ResizeFunnelRenderTexture();
+			break;
+			
+		default:
+			break;
+		}
+		#elif UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+		// Apply changes to spout server
+		ResizeSpoutRenderTexture();
+		#endif
+	}
+
+	#if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+	void SetupSyphonServer(){
+		// Add syphon components
+		gameObject.AddComponent<Syphon>();
+		gameObject.AddComponent<SyphonServerTextureCustomResolution>();
+	}
+
+	void SetupFunnelServer(){
+		// Add funnel components
+		gameObject.AddComponent<Funnel.Funnel>();
+		gameObject.GetComponent<Funnel.Funnel>().renderMode = Funnel.Funnel.RenderMode.PreviewOnGameView;
+	}
+
 	void ResizeSyphonRenderTexture(){
 		gameObject.GetComponent<SyphonServerTextureCustomResolution> ().renderWidth = renderWidth;
 		gameObject.GetComponent<SyphonServerTextureCustomResolution> ().renderHeight = renderHeight;
 		gameObject.GetComponent<SyphonServerTextureCustomResolution> ().createOrResizeRenderTexture ();
 	}
+	
+	void ResizeFunnelRenderTexture(){
+		gameObject.GetComponent<Funnel.Funnel> ().screenWidth = renderWidth;
+		gameObject.GetComponent<Funnel.Funnel> ().screenHeight = renderHeight;
+	}
 
+	#elif UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+	void SetupSpoutServer(){
+		// Create a new camera for Spout, based on this camera
+		// because Spout can't send a render texture and show the scene in app window with the same camera
+		// This instantiation is made in Start function, to prevent cloned camera 
+		// to replicate itself through this script before it is destroyed.
+		GameObject spoutCamera = Instantiate(gameObject);
+		
+		// Remove useless cloned components
+		Destroy(spoutCamera.GetComponent<SyphonSpoutServer>());
+		Destroy(spoutCamera.GetComponent<AudioListener>());
+		Destroy(spoutCamera.GetComponent<GUILayer>());
+		
+		// Set name and tag 
+		spoutCamera.name = "Camera Spout Sender";
+		spoutCamera.tag = "SpoutCamera";
+		
+		// Make spout sender camera child of main camera
+		spoutCamera.transform.parent = this.transform;
+		
+		// Add spout components 
+		gameObject.AddComponent<Spout.Spout>();	// Instantiate Spout script on main camera to prevent crash when exiting app
+		spoutCamera.AddComponent<Spout.SpoutSender>();
+		spoutCamera.AddComponent<Spout.InvertCamera>();
+		
+		// Setup spout components
+		spoutCamera.GetComponent<Spout.SpoutSender>().sharingName = Application.productName;
+		// Create a new render texture
+		RenderTexture targetTexture = new RenderTexture(renderWidth, renderHeight, 24);
+		// Set it to the camera 
+		spoutCamera.GetComponent<Camera>().targetTexture = targetTexture;
+		// And set it to Spout
+		spoutCamera.GetComponent<Spout.SpoutSender>().texture = targetTexture;
+	}
+
+	void ResizeSpoutRenderTexture(){
+		GameObject spoutChild = null;
+		foreach (Transform child in gameObject.transform) {
+			if(child.CompareTag("SpoutCamera")){
+				spoutChild = child.gameObject;
+				break;
+			}
+		}
+		Spout.SpoutSender spoutSender = spoutChild.GetComponent<Spout.SpoutSender>();
+		Camera camera = spoutChild.GetComponent<Camera>();
+		
+		// Disable Spout while updating targettexture to prevent crash
+		spoutSender.enabled = false;
+		
+		// Create a new render texture because we can't reallocate an already existing one
+		// Store camera target texture in temp variable to be able to release it while not in use by the camera
+		RenderTexture targetTexture = camera.targetTexture;
+		camera.targetTexture = null;
+		targetTexture.Release();
+		targetTexture = new RenderTexture(renderWidth, renderHeight, 24);
+		// Set it to the camera 
+		camera.targetTexture = targetTexture;
+		// Set it to spout sender
+		spoutSender.texture = targetTexture;
+		
+		// Enable spout sender
+		spoutSender.enabled = true;
+	}
+	#endif
+	
 	void SaveSettings(){
 		PlayerPrefs.SetInt ("renderWidth", renderWidth);
 		PlayerPrefs.SetInt ("renderHeight", renderHeight);
