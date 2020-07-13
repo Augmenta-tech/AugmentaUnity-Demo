1414500cbb6c8dbf51162dd1b2e32ec9a97b08ba
BenoitArbelot <benoit.arbelot@gmail.com>
2020-07-13 17:14:32 +0200

    Added VideoOutput from external camera example. Added debug textures to videoOut examples.

---
+++ Assets/Scripts/AugmentaVideoOutputMaterialBinder.cs
@@ -0,0 +1,29 @@
+﻿using Klak.Spout;
+using Augmenta;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AugmentaVideoOutputMaterialBinder : MonoBehaviour
+{
+    public Material material;
+    public string propertyName;
+    public AugmentaVideoOutput augmentaVideoOutput;
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        augmentaVideoOutput.videoOutputTextureUpdated += OnVideoOutputTextureUpdated;
+        OnVideoOutputTextureUpdated();
+    }
+
+	void OnDisable() {
+
+        augmentaVideoOutput.videoOutputTextureUpdated -= OnVideoOutputTextureUpdated;
+    }
+
+	void OnVideoOutputTextureUpdated() {
+
+        material.SetTexture(propertyName, augmentaVideoOutput.videoOutputTexture);
+	}
+}
