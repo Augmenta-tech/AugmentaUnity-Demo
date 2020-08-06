9cdf99972a543367375b32d8efa368cf4fd1b309
BenoitArbelot <benoit.arbelot@gmail.com>
2020-08-06 19:02:55 +0200

    Added VFXGraph scene example.

---
+++ Assets/Scenes/8 - AugmentaToVFXGraph/Scripts/AugmentaSandManager.cs
@@ -0,0 +1,75 @@
+﻿using Augmenta;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class AugmentaSandManager : MonoBehaviour
+{
+    public AugmentaManager augmentaManager;
+    public VisualEffect vfx;
+
+	private Vector3 _defaultPosition = 1000000.0f * Vector3.one;
+
+	// Update is called once per frame
+	private void OnEnable() {
+
+		InitializePositions();
+
+		augmentaManager.augmentaObjectUpdate += OnAugmentaObjectUpdate;
+		augmentaManager.augmentaObjectLeave += OnAugmentaObjectLeave;
+	}
+
+	void OnDisable() {
+
+		augmentaManager.augmentaObjectUpdate -= OnAugmentaObjectUpdate;
+		augmentaManager.augmentaObjectLeave -= OnAugmentaObjectLeave;
+	}
+
+	void InitializePositions() {
+
+		vfx.SetVector3("_AugmentaObjectPosition0", _defaultPosition);
+		vfx.SetVector3("_AugmentaObjectPosition1", _defaultPosition);
+		vfx.SetVector3("_AugmentaObjectPosition2", _defaultPosition);
+	}
+
+	void OnAugmentaObjectUpdate(AugmentaObject augmentaObject, AugmentaDataType augmentaDataType) {
+
+		if (augmentaDataType != AugmentaDataType.Main)
+			return;
+
+		switch (augmentaObject.oid) {
+			case 0:
+				vfx.SetVector3("_AugmentaObjectPosition0", augmentaObject.worldPosition2D);
+				break;
+
+			case 1:
+				vfx.SetVector3("_AugmentaObjectPosition1", augmentaObject.worldPosition2D);
+				break;
+
+			case 2:
+				vfx.SetVector3("_AugmentaObjectPosition2", augmentaObject.worldPosition2D);
+				break;
+		}
+	}
+
+	void OnAugmentaObjectLeave(AugmentaObject augmentaObject, AugmentaDataType augmentaDataType) {
+
+		if (augmentaDataType != AugmentaDataType.Main)
+			return;
+
+		switch (augmentaObject.oid) {
+			case 0:
+				vfx.SetVector3("_AugmentaObjectPosition0", _defaultPosition);
+				break;
+
+			case 1:
+				vfx.SetVector3("_AugmentaObjectPosition1", _defaultPosition);
+				break;
+
+			case 2:
+				vfx.SetVector3("_AugmentaObjectPosition2", _defaultPosition);
+				break;
+		}
+	}
+}
--- README.md
+++ README.md
@@ -83,6 +83,9 @@ In this example, a custom object prefab is used to make a simple scene with squi
 
 In this example, the Augmenta person data is send to a ripple shader in order to have the shader creates ripples under the Augmenta persons.
 
+### 7 - AugmentaToVFXGraph
+
+In this example, the Augmenta person data is send to a VFXGraph in order to make the sand particle react to the oldest 3 persons in the scene.
 
 Augmenta Documentation
 -------------
